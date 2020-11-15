1c9b20f8d453cc05bf8479b76906d8c34bd8d8c6
Bee-Jess <70593797+Bee-Jess@users.noreply.github.com>
2020-11-15 03:10:38 -0600

    we workin

--- p5/Unit2/multsound/sketch.js
+++ p5/Unit2/multsound/sketch.js
@@ -1,10 +1,10 @@
 let state = 0;
 let song1, song2, song3;
 
-function preload(){
-  song1= loadSound("assets/bensound-love.mp3");
-  song2= loadSound("assets/bensound-november.mp3");
-  song3= loadSound("assets/bensound-scifi.mp3");
+function preload() {
+  song1 = loadSound("assets/bensound-love.mp3");
+  song2 = loadSound("assets/bensound-november.mp3");
+  song3 = loadSound("assets/bensound-scifi.mp3");
   song1.loop();
   song1.pause();
   song2.loop();
@@ -25,14 +25,34 @@ function draw() {
 
     case 0:
       text("0", 100, 100);
+      song1.play();
+      state = 1;
       break;
 
     case 1:
-      text("1", 100, 100);
+      text("Listen to song 1", 100, 100);
       break;
 
     case 2:
       text("2", 100, 100);
+      song2.play();
+      state = 3;
+      break;
+
+    case 3:
+      text("Listen to song 2", 100, 100);
+      break;
+
+    case 4:
+      text("2", 100, 100);
+      song3.play();
+      state = 5;
+      break;
+
+    case 5:
+      text("Listen to song 3", 100, 100);
+      song2.play();
+      state = 3;
       break;
 
   }
