b536bfc46627a2a721ac8e965fcf362739bf1cb7
Bee-Jess <70593797+Bee-Jess@users.noreply.github.com>
2020-12-04 22:14:08 -0600

    pl

--- p5/Unit2/joke1/sketch.js
+++ p5/Unit2/joke1/sketch.js
@@ -1,7 +1,29 @@
+let state = 0;
+
 function setup() {
   createCanvas(500, 500);
+  textAlign(CENTER);
+  textSize(16);
 }
 
 function draw() {
+  switch (state) {
 
+    case 0:
+      background('green');
+      text("What is the favorite color of a wind turbine?", 250, 250);
+      break;
+
+    case 1:
+      background('blue');
+      text("blew", 250, 250);
+      break;
+  }
+}
+
+function mouseReleased() {
+  state++;
+  if (state > 1) {
+    state = 0;
+  }
 }
