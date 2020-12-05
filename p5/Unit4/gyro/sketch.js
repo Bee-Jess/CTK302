c80d393cdb7bea93d1f73d5b949c148120d8bc88
Bee-Jess <70593797+Bee-Jess@users.noreply.github.com>
2020-12-05 10:51:54 -0600

    i

--- p5/Unit4/gyro/sketch.js
+++ p5/Unit4/gyro/sketch.js
@@ -41,25 +41,12 @@ function draw() {
 
 
   // Text commands that display debugging data
-  textAlign(LEFT);
-  textSize(20);
-  fill('black');
-  text("orientation data:", 25, 25);
-  textSize(15);
-  text("alpha: " + alpha, 25, 50);
-  text("beta: " + beta, 25, 70);
-  text("gamma: " + gamma, 25, 90);
-  textSize(20);
-  text("acceleration data:", 25, 125);
-  textSize(15);
-  text("x = " + x.toFixed(2), 25, 150); // .toFixed means just show (x) decimal places
-  text("y = " + y.toFixed(2), 25, 170);
-  text("z = " + z.toFixed(4), 25, 190);
+  //textAlign(LEFT);
 
   // Text that makes CTK type in the background
   fill('white');
   noStroke();
-  textSize(20);
+  textSize(100);
   textAlign(CENTER);
   text("BOO!", width / 2, height / 2);
 
