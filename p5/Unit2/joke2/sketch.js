91211318e048b8cdbc6c1018d767817221930233
Bee-Jess <70593797+Bee-Jess@users.noreply.github.com>
2020-12-04 22:15:31 -0600

    aoaoaoaooaoaoaoaoaooa

--- p5/Unit2/joke2/sketch.js
+++ p5/Unit2/joke2/sketch.js
@@ -1,7 +1,32 @@
+let timer = 0;
+let state = 0;
+
 function setup() {
   createCanvas(500, 500);
+  textSize(32);
+  textAlign(CENTER);
+  rectMode(CENTER);
 }
 
 function draw() {
 
+  switch (state) {
+
+    case 0:
+      background('red');
+      text("What do you get if you cross a pig and a cactus?", 250, 250, 350, 400);
+      timer = timer + 1;
+      if (time > 5 * 60) {
+        state = 1;
+        timer = 0;
+
+      }
+
+      break;
+
+    case 1:
+      background('blue');
+      text("a porkypine", 250, 250, 350, 400);
+      break;
+  }
 }
